d1895bb5d8c9f8cf9a0469f2e08d8e9a3bff5f7b
ratul <ratul.sraj@gmail.com>
2023-08-19 18:17:25 -0400

    added users

--- blogposts/settings.py
+++ blogposts/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "blog.apps.BlogConfig",
     "users.apps.UsersConfig",
+    "crispy_forms",
+    "crispy_bootstrap4",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,8 +120,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+MEDIA_URL="/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
--- users/views.py
+++ users/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 # Create your views here.
 
 def register(req):
-    if (req.method =="POST"):
-        form = UserCreationForm(req.POST)
+    if req.method == "POST":
+        form = UserRegisterForm(req.POST)
         if form.is_valid():
-            print("the form is valid")
+            form.save()
             username = form.cleaned_data.get("username")
-            messages.success(req,f"Account created for {username} !")
-            return redirect("blog-home")
+            messages.success(req, f"Your Account Has been created  Please Log in !")
+            return redirect("login")
     else:
-        form = UserCreationForm()
-        return render(req,"users/register.html", {"form":form})
+        form = UserRegisterForm()
+    return render(req, "users/register.html", {"form": form})
+
+@login_required()
+def profile(req):
+    return render(req,"users/profile.html")
