b42a4980254e89db22c2976864d61db8f79a4c38
ratul <ratul.sraj@gmail.com>
2023-08-25 02:37:14 -0400

    added users

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,13 @@ from typing import List, Dict
 
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+)
 
 
 # Create your views here.
@@ -11,5 +18,39 @@ def home(req):
     return render(req, 'blog/home.html', context)
 
 
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+
 def about(req):
-    return render(req, "blog/about.html",{"title": "About"})
+    return render(req, "blog/about.html", {"title": "About"})
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm,ProfileUpdateForm,UserUpdateForm
 # Create your views here.
 
 def register(req):
@@ -18,4 +18,20 @@ def register(req):
 
 @login_required()
 def profile(req):
-    return render(req,"users/profile.html")
+    if req.method == "POST":
+        u_form = UserUpdateForm(req.POST,instance=req.user)
+        p_form = ProfileUpdateForm(req.POST,
+                                   req.FILES,
+                                   instance=req.user.profile
+                                   )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(req, f"Your Account Has been updated  Please Log in !")
+            return redirect("profile")
+
+    else:
+        u_form = UserUpdateForm(instance=req.user)
+        p_form = ProfileUpdateForm(instance=req.user.profile)
+    context = {"u_form":u_form,"p_form":p_form}
+    return render(req,"users/profile.html",context)
