2841ef7a146332e58b9ba97bcff5c7d2f169d271
Ratul S Raj <71872576+ratul-sraj@users.noreply.github.com>
2023-08-29 12:02:52 -0400

    added users

--- blogposts/settings.py
+++ blogposts/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-18sdga!x)f1&b!h_n2-b@gdi(z#!x449s#ucqe%66#y_4gvv^m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myblogpostsapp-74604634872d.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['myblogpostsapp-74604634872d.herokuapp.com']
 
 # Application definition
 
@@ -112,9 +112,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_URL = 'staticfiles/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'static/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
